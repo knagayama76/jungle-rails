8f50c408b659ca090703a337fdf4812462f76e20
knagayama76 <knagayama76>
2022-03-10 18:48:37 -0500

    create categories route

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,4 @@
-class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+class Admin::DashboardController < Admin::BaseController
   def show
     @products_total = Product.count
     @categories_total = Category.count
@@ -8,11 +7,3 @@ class Admin::DashboardController < ApplicationController
 
 end
 
-
-# users = User.where(name: 'David', occupation: 'Code Artist').order(created_at: :desc)
-
-# Display a count of how many products are in the database
-# Display a count of how many categories are in the database
-
-# Tips
-# We could use AR models to query the database directly from the ERB views. However a better, more MVC approach to this is to let the controller fetch the data and pass it to the template. In other words, set instance variables in the controller action with values, and then render those instance variables in the ERB template
